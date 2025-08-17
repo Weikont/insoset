7a2f427b9caa7e4ca972db1388d2450a6026b88d
kidezp <103308988+Weikont@users.noreply.github.com>
2025-08-17 22:26:11 +0300

    Delete backend directory

--- backend/routes/wanted.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const upload = require('../config/multer');
-
-router.post('/persons', authenticate, upload.single('photo'), async (req, res) => {
-  // Ваш существующий код обработчика
-});
-
-module.exports = router;
